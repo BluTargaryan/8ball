6c2c5be72e50c82169a81d4428907b47091d7379
BluTargaryan <opeyemiyolo@gmail.com>
2023-02-02 22:53:39 +0100

    Made an 8 ball app

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,70 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  const BallPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        backgroundColor: Colors.blue.shade900,
+        title: Text('8-Ball'),
+      ),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  const Ball({Key? key}) : super(key: key);
+
+  @override
+  State<Ball> createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 20, horizontal: 0),
+      child: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Container(
+              child: Text(
+                'Ask any question, then click the image to get your answer',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              width: 300,
+            ),
+            SizedBox(
+              height: 100,
+            ),
+            TextButton(
+              child: Image.asset('images/ball${ballNumber}.png'),
+              onPressed: () {
+                setState(() {
+                  ballNumber = (Random().nextInt(5) + 1);
+                });
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
